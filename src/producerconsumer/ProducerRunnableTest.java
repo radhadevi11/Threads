bbc72b61e6e68697baf7dace86f4f22ac4d78d5f
radhadevi11 <radhadevi16isr033@gmail.com>
2019-05-23 21:36:18 +0530

    ProducerRunnableTest with Mock version II

--- src/producerconsumer/ProducerRunnableTest.java
+++ src/producerconsumer/ProducerRunnableTest.java
@@ -2,31 +2,34 @@ package producerconsumer;
 
 import org.junit.Test;
 import org.junit.runner.RunWith;
+import org.mockito.InjectMocks;
 import org.mockito.Mock;
 import org.mockito.Mockito;
+import org.mockito.Spy;
 import org.mockito.runners.MockitoJUnitRunner;
-
 import java.util.LinkedList;
+import static org.mockito.Mockito.*;
 
 @RunWith(MockitoJUnitRunner.class)
 
 public class ProducerRunnableTest {
-
+    @InjectMocks
+    @Spy
+    private ProducerRunnable producerRunnable ;
     @Mock
     private LinkedList<Double> numbers ;
 
+
     @Test
     public void testProducerRunnable() throws InterruptedException {
-        Runnable producerRunnable = new ProducerRunnable(numbers);
-        Thread thread = new Thread(producerRunnable,"ProducerThread");
 
-        Mockito.doNothing().when(producerRunnable).wait();
-        Mockito.doNothing().when(producerRunnable).notifyAll();
+        doNothing().when(numbers).wait();
+        doNothing().when(numbers).notifyAll();
 
-        thread.start();
+        producerRunnable.run();
 
-        Mockito.verify(producerRunnable).wait();
-        Mockito.verify(producerRunnable).notifyAll();
+        Mockito.verify(numbers).wait();
+        Mockito.verify(numbers).notifyAll();
     }
 
 
